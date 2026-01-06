17288f74439acd6cb14c5cd596a764de47b269cb
anujaraired <anuj@adaired.org>
2026-01-06 18:48:03 +0530

    ficcx

--- src/@core/data/website/Homepage.ts
+++ src/@core/data/website/Homepage.ts
@@ -77,7 +77,7 @@ export const ExpectSectionData = {
   records: [
     {
       number: 7,
-      suffix: ' years',
+      suffix: '+ years',
       name: 'Industry Expertise',
       description:
         'Proven experience delivering thoughtfully crafted digital strategies designed to drive measurable results',
@@ -99,7 +99,7 @@ export const ExpectSectionData = {
     {
       number: 320,
       suffix: '+',
-      name: 'Rands Empowered Globally',
+      name: 'Bands Empowered Globally',
       description:
         'Helping brands across the globe grow visibility, credibility, trust, and sustainable success.',
     },
