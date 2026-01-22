c45303b46c429197fd7229cc0818b692069826bd
anujaraired <anuj@adaired.org>
2026-01-22 18:20:16 +0530

    fdf

---
+++ src/app/(website)/(mainWebsite)/case-studies/[slug]/data.ts
@@ -0,0 +1,120 @@
+import img from '../../../../../../public/assets/images/blogs/blog_01.png'
+import img2 from '../../../../../../public/assets/images/blogs/blog_01.png'
+import img3 from '../../../../../../public/assets/images/blogs/blog_01.png'
+
+export const data = {
+  label: 'Case',
+  Heading: `Digital Agency That
+Turns Businesses Into Brands`,
+  image: '',
+  description: [
+    {
+      name: 'Lorem Ipsum',
+      description:
+        'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact.',
+    },
+    ,
+    {
+      name: 'Lorem Ipsum',
+      description:
+        'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact.',
+    },
+    {
+      description:
+        'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact.',
+    },
+  ],
+  projectInfo: {
+    labels: [
+      {
+        lebel: 'Date',
+        description: 'Jun 20,2025',
+      },
+      {
+        lebel: 'Client',
+        description: 'Name from Which Country',
+      },
+      {
+        lebel: 'Category',
+        description: 'Development',
+      },
+    ],
+    details: [
+      {
+        description:
+          "'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience.',",
+        list: [
+          'We’re a digital agency that transforms businesses.',
+          'We’re a digital agency that transforms businesses into brands.',
+          'We’re a digital agency that transforms businesses.',
+        ],
+      },
+      {
+        description:
+          "'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience.',",
+        list: [
+          img,img3,img2
+        ],
+      },
+    ],
+  },
+  aboutTheproject: {
+    description:
+      "'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience.',",
+    details: [
+      {
+        label: 'Challange',
+        list: [
+          'We’re a digital agency that transforms businesses.',
+          'We’re a digital agency that transforms businesses into brands.',
+          'We’re a digital agency that transforms businesses.',
+        ],
+      },
+      {
+        label: 'Solutions',
+        list: [
+          'We’re a digital agency that transforms businesses.',
+          'We’re a digital agency that transforms businesses into brands.',
+          'We’re a digital agency that transforms businesses.',
+        ],
+      },
+    ],
+  },
+  projectInformation: {
+    data: [
+      {
+        icon: 'c',
+        label: 'We’re a digital agency',
+        description:
+          'We’re a digital agency that transforms businesses into brands. ',
+      },
+      {
+        icon: 'c',
+        label: 'Challange',
+        description:
+          'We’re a digital agency that transforms businesses into brands. ',
+      },
+      {
+        icon: 'c',
+        label: 'Challange',
+        description:
+          'We’re a digital agency that transforms businesses into brands. ',
+      },
+    ],
+    details: [
+      {
+        description:
+          "'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience.',",
+        image: '',
+      },
+      {
+        description:
+          "'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience.',",
+        image: '',
+      },
+    ],
+  },
+  solution: 'Solution:',
+  solutionDescription:
+    'We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact. We’re a digital agency that transforms businesses into brands. With the perfect blend of strategy and creativity, we help you stand out, connect with your audience, and grow with impact.',
+};
--- src/app/(website)/(mainWebsite)/case-studies/[slug]/page.tsx
+++ src/app/(website)/(mainWebsite)/case-studies/[slug]/page.tsx
@@ -13,6 +13,9 @@ import MaxWidthWrapper from '@/app/(website)/components/MaxWidthWrapper';
 import SaveAndCancel from '@/app/(website)/common/SaveAndCancel';
 import { LuCircleCheckBig } from 'react-icons/lu';
 import Heading from '@/app/(website)/common/Heading';
+import img from '../../../../../../public/assets/images/blogs/blog_01.png';
+import { data } from './data';
+import { IoCheckmarkCircle } from 'react-icons/io5';
 
 interface CaseStudy {
   title: string;
@@ -23,6 +26,8 @@ interface CaseStudy {
 
 const CaseStudies = () => {
   const { slug } = useParams(); // ✅ SLUG HERE
+  const { Heading, label, description, image, projectInfo, aboutTheproject } =
+    data ?? {};
   const [caseStudies, setCaseStudies] = useState<CaseStudy | null>(null);
   const getData = async () => {
     const res = await axios.get(
@@ -34,30 +39,121 @@ const CaseStudies = () => {
     getData();
   }, []);
 
-  console.log(caseStudies, 'caseStudies>>>>>>>>>>>>>>>>>jds');
-  const { title, description, image } = caseStudies ?? {};
-  console.log(title, description, 'title, description');
+  const { title } = caseStudies ?? {};
   return (
     <>
       <PageBanner subTitle={'SEO'} title="CASE STUDY" />
       <Suspense fallback={<p>Loading feed...</p>}>
         <MaxWidthWrapper className="py-[3rem] lg:py-[4rem] xl:py-[6rem]">
           <div>
-            <Heading subTitle={'CASE '} title={title ?? ''} span={''} />
-            <div className='flex gap-2 justify-between'>
-              <div className="relative h-[20rem] w-full">
-                {image && (
-                  <div className="relative h-[20rem] w-full">
-                    <Image
-                      src={image}
-                      alt={title || 'case study image'}
-                      fill
-                      className="mt-2 object-contain"
-                    />
-                  </div>
-                )}
-                <div>
-                  <h2>dfd</h2>
+            {/* <Heading  /> */}
+            <div>
+              <span>Case</span>
+              <h2>
+                Digital Agency That <br></br> Turns Businesses Into Brands
+              </h2>
+            </div>
+            <div className="grid grid-cols-2 gap-[2rem]">
+              {img && (
+                <div className="relative h-[20rem] w-full">
+                  <Image
+                    src={img}
+                    alt={'case study image'}
+                    fill
+                    className="mt-2 object-contain"
+                  />
+                </div>
+              )}
+              <div>
+                {description?.map((para) => {
+                  return (
+                    <div className="my-[1rem]">
+                      <h3 className="pb-3">{para?.name}</h3>
+                      <p className="">{para?.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div>
+              <div className="flex gap-3">
+                {projectInfo?.labels?.map((item) => {
+                  return (
+                    <div className="flex gap-4">
+                      <div className="my-auto h-5 w-0.5 bg-[#1B5A96]"></div>
+                      <p className="font-semibold text-[#000000]">
+                        {item?.lebel}
+                      </p>
+                      <p className="text-[#797979]">{item?.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="py-[1rem]">
+                {projectInfo?.details?.map((item) => {
+                  return (
+                    <div className="my-4 gap-4 space-y-4">
+                      <p className="flex">{item?.description}</p>
+                      <div className="text-[#797979]">
+                        {item?.list?.map((x) => {
+                          return (
+                            <div className="flex gap-2 py-1">
+                              <span className="my-auto">
+                                <IoCheckmarkCircle
+                                  fontSize={22}
+                                  className={'text-[#00BE29]'}
+                                />
+                              </span>
+                              <p> {'difgjds'}</p>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div>
+              <div className="flex gap-3">
+                {projectInfo?.labels?.map((item) => {
+                  return (
+                    <div className="flex gap-4">
+                      <div className="my-auto h-5 w-0.5 bg-[#1B5A96]"></div>
+                      <p className="font-semibold text-[#000000]">
+                        {item?.lebel}
+                      </p>
+                      <p className="text-[#797979]">{item?.description}</p>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="py-[1rem]">
+                <h2>About the Project</h2>
+                <p className="flex">{aboutTheproject?.description}</p>
+                <div className=" flex justify-between">
+                  {aboutTheproject?.details?.map((item) => {
+                    return (
+                      <div className="relative rounded-[20px] bg-[#F8FBFF] p-[2rem]">
+                        <h3 className='absolute top-[-1rem] left-[2rem] bg-[#1B5A96] text-[#FFFFFF] px-[2rem] py-[0.125rem] rounded-full'>{item?.label}</h3>
+                        <div className="text-[#797979]">
+                          {item?.list?.map((x) => {
+                            return (
+                              <div className="flex gap-2 py-1">
+                                <span className="my-auto">
+                                  <IoCheckmarkCircle
+                                    fontSize={22}
+                                    className={'text-[#00BE29]'}
+                                  />
+                                </span>
+                                <p> {x}</p>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
