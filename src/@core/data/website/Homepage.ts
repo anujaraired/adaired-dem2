50cf189d98a0d98ebdf9c6fec92571c08f313332
anujaraired <anuj@adaired.org>
2026-01-23 13:38:39 +0530

    img upadtred

--- src/@core/data/website/Homepage.ts
+++ src/@core/data/website/Homepage.ts
@@ -66,7 +66,8 @@ import LinkBuilding from '../../../../public/assets/icons/service/LinkBuilding.p
 import shoppyfy from '../../../../public/assets/icons/service/shoppyfy.png';
 import TikTokAds from '../../../../public/assets/icons/service/TikTokAds.png';
 import LinkedInAds from '../../../../public/assets/icons/service/LinkedInAds.png';
-import digital_marketing from '../../../../public/assets/images/service/digital_marketing.png';
+// import digital_marketing from '../../../../public/assets/images/service/digital_marketing.png';
+import digital_marketing from '../../../../public/assets/images/service/New/Digital Marketing.png';
 import google_business from '../../../../public/assets/images/service/google_business.png';
 import App_development from '../../../../public/assets/images/service/App_development.png';
 import WhiteLabelFacebookAds from '../../../../public/assets/icons/service/WhiteLabelFacebookAds.png';
