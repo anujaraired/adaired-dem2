b15d37f4bb486755ef74148fe89db0dba0987fcd
anujaraired <anuj@adaired.org>
2026-01-28 18:58:25 +0530

    fdf

--- src/app/(website)/components/home/Service.tsx
+++ src/app/(website)/components/home/Service.tsx
@@ -171,7 +171,7 @@ const Service = () => {
                     >
                       <div className="flex w-full items-center justify-between gap-3 1360:gap-1">
                         <h3
-                          className={`my-auto text-[18px] leading-[1.35rem] font-medium 1360:text-[18px] 1600:text-[18px] 3xl:text-[18px] ${
+                          className={`my-auto text-[18px] font-medium leading-[1.35rem] 1360:text-[18px] 1600:text-[18px] 3xl:text-[18px] ${
                             activeTab === idx
                               ? 'text-[#ffffff]'
                               : 'text-[#000000]'
@@ -231,7 +231,7 @@ const Service = () => {
                             src={image ?? service.img}
                             fill
                             alt={service.title}
-                            className="object-fill"
+                            className="object-cover"
                             priority
                           />
                         </div>
