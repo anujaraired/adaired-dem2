47c39f62f2380a21f5cd460a3d340383525a78d6
anujaraired <anuj@adaired.org>
2026-02-04 15:57:52 +0530

    iyui

--- src/app/(website)/(mainWebsite)/services/[slug]/AISEOResult.tsx
+++ src/app/(website)/(mainWebsite)/services/[slug]/AISEOResult.tsx
@@ -39,7 +39,6 @@ const AISEOResult = ({ aiseoResult }: any) => {
             className={`relative h-[480px] w-[42%] rounded-[20px] transition-all duration-1000 ${isVisible ? 'translate-x-0 opacity-100' : '-translate-x-16 opacity-0'}`}
           >
             <>
-            
               <Image
                 src={image}
                 fill
