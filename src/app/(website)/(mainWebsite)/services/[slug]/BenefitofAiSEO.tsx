da0f9f8f5c987f86f5dd7932de9d05cd59f1c800
anujaraired <anuj@adaired.org>
2026-02-05 12:48:45 +0530

    uopdared slide

--- src/app/(website)/(mainWebsite)/services/[slug]/BenefitofAiSEO.tsx
+++ src/app/(website)/(mainWebsite)/services/[slug]/BenefitofAiSEO.tsx
@@ -2,6 +2,7 @@ import { useInViewOnce } from '@/@core/hooks/useInViewOnce';
 import Heading from '@/app/(website)/common/Heading';
 import MaxWidthWrapper from '@/app/(website)/components/MaxWidthWrapper';
 import React from 'react';
+import CustomSlider from './CustomSlider';
 const BenefitofAiSEO = ({ benefitofAiSeo }: any) => {
   const { ref, isVisible } = useInViewOnce<HTMLDivElement>(0.3);
 
@@ -28,30 +29,8 @@ const BenefitofAiSEO = ({ benefitofAiSeo }: any) => {
             })}
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-[1rem] pt-[3rem] lg:grid-cols-3">
-          {benefitofAiSeo?.list?.slice(0, 3)?.map((item: any, idx: number) => {
-            return (
-              <div
-                key={idx}
-                className={`rounded-[20px] border-[1px] border-[#FB9100]/25 bg-[#FFFFFF] p-[2rem] transition-all duration-1000 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}`}
-                style={{
-                  transitionDelay: `${idx * 280}ms`, // 👈 stagger here
-                }}
-              >
-                <h2 className="w-fit rounded-[20px] bg-[#FB9100]/20 p-2 px-6 text-[#FB9100]">{`0${idx + 1}`}</h2>
-                <h3 className="pb-[1rem] pt-[2rem] text-[#FB9100]">
-                  {item?.name}
-                </h3>
-                <div className="">
-                  {item?.description?.map((item: any) => {
-                    return <p className="py-2">{item}</p>;
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
       </MaxWidthWrapper>
+      <CustomSlider data={benefitofAiSeo?.list} />
     </div>
   );
 };
---
+++ src/app/(website)/(mainWebsite)/services/[slug]/CustomSlider.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+export default function CustomSlider({ data }: any) {
+  console.log(data, 'dataidsfdsf');
+  return (
+    <div className="mx-auto w-[100%] overflow-hidden py-12">
+      <Swiper
+        loop
+        centeredSlides
+        slidesPerView="auto"
+        spaceBetween={24}
+        slidesOffsetBefore={240}
+        slidesOffsetAfter={240}
+      >
+        {data?.length > 0 &&
+          data?.map((item: any, idx: number) => (
+            <SwiperSlide key={item.id} className="w-[100%] lg:!w-[40%]">
+              <div
+                className={`h-[26rem] rounded-[20px] border-[1px] border-[#FB9100]/25 bg-[#FFFFFF] p-[2rem] transition-all duration-1000`}
+              >
+                <h2 className="w-fit rounded-[20px] bg-[#FB9100]/20 p-2 px-6 text-[#FB9100]">{`0${idx + 1}`}</h2>
+                <h3 className="pb-[1rem] pt-[2rem] text-[#FB9100]">
+                  {item?.name}
+                </h3>
+                <div className="">
+                  {item?.description?.map((item: any) => {
+                    return <p className="py-2">{item}</p>;
+                  })}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+}
