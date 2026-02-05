97561eb47813935bd5057cf880aedab47fcdd59d
Gursimran78826 <gursimranpreet@adaired.org>
2026-02-05 13:01:09 +0530

    Updated services

--- src/app/(website)/(mainWebsite)/services/[slug]/WhatMakesAdaired.tsx
+++ src/app/(website)/(mainWebsite)/services/[slug]/WhatMakesAdaired.tsx
@@ -25,22 +25,51 @@ const WhatMakesAdaired = ({ whatMkeDeferent }: any) => {
           {whatMkeDeferent?.list?.map((item: any, idx: number) => {
             const isOrange = idx % 2 === 0;
             return (
+              // <div
+              //   style={{
+              //     transitionDelay: `${idx * 120}ms`,
+              //   }}
+              //   className={`my-[2rem] flex justify-between rounded-[20px] p-[2rem] transition-all duration-700 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} ${isOrange ? 'bg-[#FB9100]/5' : 'bg-[#F9F9F9]'}`}
+              // >
+              //   <div className="my-auto w-[100%] lg:w-[50%]">
+              //     <h3>{item?.name}</h3>
+              //     <div className="pt-[1rem]">
+              //       {item?.description?.map((desc: any) => {
+              //         return <p className="my-[1rem]">{desc}</p>;
+              //       })}
+              //     </div>
+              //   </div>
+              //   <div className="flex w-[100%] justify-end justify-items-end lg:w-[40%]">
+              //     <Image src={item?.img} alt="if" />
+              //   </div>
+              // </div>
+
+
+
               <div
-                style={{
-                  transitionDelay: `${idx * 120}ms`,
-                }}
-                className={`my-[2rem] flex justify-between rounded-[20px] p-[2rem] transition-all duration-700 ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} ${isOrange ? 'bg-[#FB9100]/5' : 'bg-[#F9F9F9]'}`}
+                style={{ transitionDelay: `${idx * 120}ms` }}
+                className={`my-[2rem] grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-0 rounded-[20px] p-[2rem] transition-all duration-700 ${isVisible ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
+                  } ${isOrange ? "bg-[#FB9100]/5" : "bg-[#F9F9F9]"}`}
               >
-                <div className="my-auto w-[100%] lg:w-[50%]">
+                {/* IMAGE */}
+                <div className="order-1 lg:order-2 flex justify-center lg:justify-end">
+                  <Image
+                    src={item?.img}
+                    alt={item?.name}
+                    className="w-full lg:w-auto h-auto"
+                  />
+                </div>
+
+                {/* TEXT */}
+                <div className="order-2 flex flex-col justify-center items-center lg:items-start lg:order-1 w-full">
                   <h3>{item?.name}</h3>
                   <div className="pt-[1rem]">
-                    {item?.description?.map((desc: any) => {
-                      return <p className="my-[1rem]">{desc}</p>;
-                    })}
+                    {item?.description?.map((desc: any, i: number) => (
+                      <p key={i} className="my-[1rem]">
+                        {desc}
+                      </p>
+                    ))}
                   </div>
-                </div>
-                <div className="flex w-[100%] justify-end justify-items-end lg:w-[40%]">
-                  <Image src={item?.img} alt="if" />
                 </div>
               </div>
             );
