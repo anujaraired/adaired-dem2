4e93bbe0a035a91e12136d81440d7e920a29b7d9
Gursimran78826 <gursimranpreet@adaired.org>
2026-02-19 10:41:43 +0530

    "updated"

--- src/app/(website)/(mainWebsite)/services/[slug]/data.ts
+++ src/app/(website)/(mainWebsite)/services/[slug]/data.ts
@@ -370,7 +370,7 @@ export const SERVICES_DATA = {
     },
     importantToBussiness: {
       isVariant: '01',
-      breakIndex: 4,
+      breakIndex: 6,
       isVisible: true,
       heading: 'Why is SEO Important to Your Business?',
       buttonName: '',
@@ -3030,7 +3030,7 @@ brand appears in AI summaries and citations.`,
 
     importantToBussiness: {
       isVariant: '01',
-      breakIndex: 4,
+      breakIndex: 6,
       isVisible: true,
       heading: 'Local SEO Services Designed to Grow Your Business',
       buttonName: 'Get Your Free Local SEO Audit',
@@ -4541,7 +4541,7 @@ brand appears in AI summaries and citations.`,
 
     importantToBussiness: {
       isVariant: '01',
-      breakIndex: 4,
+      breakIndex: 8,
       isVisible: true,
       heading: 'What Will Our White Label SEO Services Cover?',
       description: [
@@ -5828,6 +5828,7 @@ brand appears in AI summaries and citations.`,
 
     importantToBussiness: {
       isVisible: false,
+      breakIndex: 5,
       heading: 'What Will Our White Label SEO Services Cover?',
       description: [
         `Being a white-label SEO services agency, we are intended to help agencies throughout the SEO lifecycle while remaining brand neutral.`,
