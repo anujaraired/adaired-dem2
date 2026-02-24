deb80a9216667d656e4ea0b2a6c2ac1c5030a0b6
akshay-backend-dev <akshay.mittal@adaired.org>
2026-02-24 13:03:02 +0530

    white label paid ads fixes

--- src/app/(website)/(mainWebsite)/services/[slug]/BookAConsultation.tsx
+++ src/app/(website)/(mainWebsite)/services/[slug]/BookAConsultation.tsx
@@ -18,18 +18,17 @@ const BookAConsultation = ({ buildlinks }: any) => {
         //   className={`${buildlinks?.isBgWhite ? 'bg-gradient-to-b from-black to-[#051C40]' : 'bg-[#FFFCF8]'} py-[3rem] lg:py-[2rem]`}
         // >
         <div
-          className={`py-[3rem] lg:py-[2rem] ${
-            buildlinks?.bgImg
+          className={`py-[3rem] lg:py-[2rem] ${buildlinks?.bgImg
               ? ''
               : buildlinks?.isBgWhite
                 ? 'bg-gradient-to-b from-black to-[#051C40]'
                 : 'bg-[#FFFCF8]'
-          }`}
+            }`}
           style={
             buildlinks?.bgImg
               ? {
-                  backgroundImage: `url(${buildlinks.bgImg?.src})`,
-                }
+                backgroundImage: `url(${buildlinks.bgImg?.src})`,
+              }
               : {}
           }
         >
@@ -158,14 +157,19 @@ const BookAConsultation = ({ buildlinks }: any) => {
               className={`order-2 my-auto flex flex-col items-center transition-all duration-1000 lg:order-1 lg:items-start ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-12 opacity-0'}`}
             >
               <h1
-                className={`font-normal text-[#FFFFFF] lg:text-left ${buildlinks?.label && 'mb-2'}`}
+                className={`font-normal text-[#FFFFFF] lg:text-left`}
               >
                 {buildlinks?.label}
               </h1>
-              <h1 className="font-bold text-[#FFFFFF] text-center lg:text-left">
-                {buildlinks?.heading}
-              </h1>
-              <div className="py-[2rem]">
+
+              <Heading
+                breakIndex={buildlinks?.breakIndex}
+                isH1={true}
+                title={buildlinks?.heading}
+                isBgWhite={buildlinks?.isBgWhite && true}
+              />
+
+              <div className="pb-[2rem]">
                 {buildlinks?.data?.map((item: any) => {
                   return (
                     <p
@@ -194,11 +198,10 @@ const BookAConsultation = ({ buildlinks }: any) => {
             >
               <div className="relative mx-auto flex-1">
                 <div
-                  className={`mx-[1rem] my-[1rem] flex items-center justify-center transition-all duration-1000 md:mx-[0.65rem] md:my-[0.65rem] lg:mx-[2.65rem] lg:my-[2.65rem] ${
-                    isVisible
+                  className={`mx-[1rem] my-[1rem] flex items-center justify-center transition-all duration-1000 md:mx-[0.65rem] md:my-[0.65rem] lg:mx-[2.65rem] lg:my-[2.65rem] ${isVisible
                       ? 'translate-x-0 opacity-100'
                       : '-translate-x-16 opacity-0'
-                  }`}
+                    }`}
                 >
                   <Image
                     src={buildlinks?.img}
