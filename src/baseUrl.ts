8743a476972b1bf2ab45a1481bca9ebeaf1baa9d
Anuj Kumar <anuj.kumar@adaired61.com>
2026-03-03 15:07:57 +0530

    fix build issue

--- src/baseUrl.ts
+++ src/baseUrl.ts
@@ -2,3 +2,4 @@ export const Base2URL = 'http://62.212.76.178:8000/api/';
 export const BaseURL = 'http://62.212.76.178:8000/api/';
 // export const BaseURL = 'http://localhost:8000/api/';
 export const Base3URL = 'http://62.212.76.178:8000/api/';
+//test
